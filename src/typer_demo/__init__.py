e270641fb51658ea4154f529d65fffedf32c071f
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-07 14:08:49 +0100

    add dotenv support
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- src/typer_demo/__init__.py
+++ src/typer_demo/__init__.py
@@ -1,8 +1,9 @@
 from uuid import UUID
-from pydantic_settings import BaseSettings, CliApp
+from pydantic_settings import BaseSettings, CliApp, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     name: str = "World"
     uuid: UUID = UUID("12345678-1234-5678-1234-567812345678")
 
