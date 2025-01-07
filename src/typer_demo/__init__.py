09d1aabf035073a2fdd8ec0357d77109f5033b66
Jens Peder Meldgaard <jpm@halfspace.io>
2025-01-07 14:12:50 +0100

    showcase env injection
    Signed-off-by: Jens Peder Meldgaard <jpm@halfspace.io>

--- src/typer_demo/__init__.py
+++ src/typer_demo/__init__.py
@@ -4,10 +4,12 @@ from pydantic_settings import BaseSettings, CliApp, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     name: str = "World"
     uuid: UUID = UUID("12345678-1234-5678-1234-567812345678")
+    TEST_ENV: str = "default"
 
 
 if __name__ == "__main__":
     settings = CliApp.run(Settings)
-    print(f"Hello, {settings.name}!")
+    print(f"Hello, {settings.name}! UUID: {settings.uuid} | ENV: {settings.TEST_ENV}")
